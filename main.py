5e7f4f33135c27b982bf1d0c980dba4b49cc7917
Enototui <ivan_kuznecov97@mail.ru>
2022-11-22 22:34:37 +0300

    Готово

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
-# Задача 1 Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих
-# на нечётной позиции
-# spisok = [20, 30, 22, 24, 17]
-# randlist = []
-# for i in range(len(spisok)):
-#     if i % 2 != 0:
-#         randlist.append(spisok[i])
-# print(sum(randlist))
+# 1.Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день
+#выходным.
+# day = int(input('Введите день: '))
+# if day >= 1 and day < 6:
+#     print('Рабочий')
+# elif day == 6 or day == 7:
+#     print('Выходной')
+# else:
+#     print('Неверное число')
 
-# Задача 2. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент,
-# второй и предпоследний и т.д
-# my_list = [2, 3, 5, 6]
-# res = []
-# while len(my_list) > 0:
-#             name = my_list.pop(0)
-#             name_1 = my_list.pop()
-#             sum = name * name_1
-#             res.append(sum)
-# print(res)
+# 2.Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# def inputNumbers(x):
+#     xyz = ["X", "Y", "Z"]
+#     a = []
+#     for i in range(x):
+#         a.append(input(f"Введите значение {xyz[i]}: "))
+#     return a
+# def checkPredicate(x):
+#     left = not (x[0] or x[1] or x[2])
+#     right = not x[0] and not x[1] and not x[2]
+#     result = left == right
+#     return result
+#
+#
+# statement = inputNumbers(3)
+#
+# if checkPredicate(statement) == True:
+#     print(f"Утверждение истинно")
+# else:
+#     print(f"Утверждение ложно")
 
-# Задача 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное. Нельзя использовать готовые функции.
-# num = int(input('Введите число'))
-# some = ''
-# while num > 0:
-#     some = str(num % 2) + some
-#     num = num // 2
-# print(some)
+# 3.Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти
+# плоскости, в которой находится эта точка (или на какой оси она находится).
+# x = int(input('Введите координат точки Х:  '))
+# y = int(input('Введите координат точки Y:  '))
+# if x > 0 and y > 0:
+#     print('1 координатная четверть')
+# if x < 0 and y > 0:
+#     print('2 координатная четверть')
+# if x < 0 and y < 0:
+#     print('3 координатная четверть')
+# if x > 0 and y < 0:
+#     print('4 координатная четверть')
+
+# 4.Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой
+# четверти (x и y).
+# num = int(input('Введите номер четверти от 1 до 4: '))
+# if num == 1:
+#     print('от X до Y')
+# elif num == 2:
+#     print('от -X до Y')
+# elif num == 3:
+#     print('от -X до -Y')
+# elif num == 4:
+#     print('от X до -Y')
 
 
-# Задача 3. Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и
-# минимальным значением дробной части элементов.
-
-
-spisok = [1.1, 1.2, 3.3, 10.01]
-min_num = 1
-max_num = 0
-for i in spisok:
-    if (i - int(i)) <= min_num:
-        min_num = i - int(i)
-    if (i - int(i)) >= max_num:
-        max_num = i - int(i)
-print(max_num-min_num)
+# 5. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D
+# пространстве.
+import math
+import os
+x_coordinate_A = int(input('Введите X(a): '))
+y_coordinate_A = int(input('Введите Y(a): '))
+x_coordinate_B = int(input('Введите X(b): '))
+y_coordinate_B = int(input('Введите Y(b): '))
+distance = round(math.sqrt((x_coordinate_B - x_coordinate_A)**2 + (y_coordinate_B - y_coordinate_A)**2), 2)
+print(distance)
